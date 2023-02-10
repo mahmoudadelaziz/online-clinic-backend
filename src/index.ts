d68d2c19ad6423c5e1ab4d0ca31f3cea56d928a3
Amrnash <amrnashaat8@gmail.com>
2023-02-11 00:19:12 +0200

    Use user router and errorHandler

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,16 @@
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
 import morgan from "morgan";
-
+import { errorHandler } from "./utils/errorhandler";
+import { userRouter } from "./routers/user.router";
 dotenv.config();
 const app: Express = express();
 const port = process.env.PORT;
-app.use(morgan("dev"));
-app.get("/", async (req: Request, res: Response) => {
-  res.send("Express + TypeScript Server");
-});
 
+app.use(express.json());
+app.use(morgan("dev"));
+app.use("/user", userRouter);
+app.use(errorHandler);
 app.listen(port, () => {
   console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
 });
