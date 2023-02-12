c75e2467bff9be2035c51c285422ec1eecb07e07
Amrnash <amrnashaat8@gmail.com>
2023-02-13 01:01:58 +0200

    Create userlogin logic

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,8 +1,10 @@
 import { PrismaClient } from "@prisma/client";
 import jwt from "jsonwebtoken";
+import { hash, compare } from "bcryptjs";
+
 const prisma = new PrismaClient();
 
-type userData = {
+type UserSignUpData = {
   firstName: string;
   lastName: string;
   email: string;
@@ -10,7 +12,7 @@ type userData = {
   phoneNumber: string;
   username: string;
 };
-export const createUser = async (user: userData) => {
+export const createUser = async (user: UserSignUpData) => {
   // check if user already exists
   const userExists = !!(await prisma.patient.findFirst({
     where: {
@@ -20,9 +22,31 @@ export const createUser = async (user: userData) => {
   if (userExists) {
     return Promise.reject(new Error("A user with this email already exists"));
   }
+  const hasedPassword = await hash(user.password, 10);
   const newUser = await prisma.patient.create({
-    data: user,
+    data: { ...user, password: hasedPassword },
   });
   const token = jwt.sign(user, process.env.JWT_SECRET as jwt.Secret);
   return { token, username: newUser.username };
+};
+
+type UserLoginData = {
+  username: string;
+  password: string;
+};
+export const loginUser = async (userData: UserLoginData) => {
+  const user = await prisma.patient.findFirst({
+    where: {
+      username: userData.username,
+    },
+  });
+  if (!user)
+    return Promise.reject(new Error("Either username or password are wrong."));
+  //compare password
+  const isCorrectPassword = await compare(userData.password, user.password);
+  if (!isCorrectPassword)
+    return Promise.reject(new Error("Either username or password are wrong."));
+
+  const token = jwt.sign(user, process.env.JWT_SECRET as jwt.Secret);
+  return token;
 };
