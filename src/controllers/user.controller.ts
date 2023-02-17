9b3c13adb2d395ab240ecc064403b6b3dd3dc4a5
Amrnash <amrnashaat8@gmail.com>
2023-02-17 22:26:28 +0200

    Add deletuserbyid and updateuserbyid controllers

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,6 +1,6 @@
 import { NextFunction, Response, Request } from "express";
 import { validationResult } from "express-validator";
-import { createUser, loginUser } from "../services/user.service";
+import { UserService } from "../services/user.service";
 
 export const userSignup = async (
   req: Request,
@@ -16,7 +16,7 @@ export const userSignup = async (
   try {
     const { firstName, lastName, email, password, phoneNumber, username } =
       req.body;
-    const result = await createUser({
+    const result = await UserService.createUser({
       firstName,
       lastName,
       email,
@@ -24,10 +24,12 @@ export const userSignup = async (
       phoneNumber,
       username,
     });
-    res.json({
-      message: "User Signed Up Successfully",
-      user: { username: result.username, token: result.token },
-    });
+    if (result) {
+      res.json({
+        message: "User Signed Up Successfully",
+        user: { username: result.username, token: result.token },
+      });
+    }
   } catch (error) {
     next(error);
   }
@@ -46,10 +48,48 @@ export const userLogin = async (
   }
   try {
     const { username, password } = req.body;
-    const token = await loginUser({ username, password });
-    res.json({
+    const token = await UserService.loginUser({ username, password });
+    return res.json({
       message: "User Logged In Successfully",
       token,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const deleteUserById = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const id = req.params.id;
+  if (!id) {
+    return next(new Error("User id must be provided"));
+  }
+  try {
+    await UserService.deleteUserById(id);
+    return res.json({ message: "User is deleted successfully" });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const updateUserById = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const id = req.params.id;
+  if (!id) {
+    return next(new Error("User id must be provided"));
+  }
+  try {
+    const updateData = req.body;
+    const updatedUser = await UserService.updateUserById(id, updateData);
+    return res.json({
+      message: "User data is updated successfully",
+      data: { username: updatedUser.username, email: updatedUser.email },
     });
   } catch (error) {
     next(error);
