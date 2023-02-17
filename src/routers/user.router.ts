1ffe7e2b6a7f2d5b45374a8616a2d9c7b44ba778
Amrnash <amrnashaat8@gmail.com>
2023-02-17 22:25:53 +0200

    Add update and delete user routes

--- src/routers/user.router.ts
+++ src/routers/user.router.ts
@@ -1,9 +1,18 @@
 import { Router } from "express";
-import { userLogin, userSignup } from "../controllers/user.controller";
+import {
+  deleteUserById,
+  userLogin,
+  userSignup,
+  updateUserById,
+} from "../controllers/user.controller";
 import { userSignupValidators } from "../validators/userSignup.validator";
 import { userLoginValidator } from "../validators/userLogin.validator";
+import { userUpdateValidator } from "../validators/userUpdate.validator";
 const userRouter = Router();
 
 userRouter.post("/signup", ...userSignupValidators, userSignup);
 userRouter.post("/login", ...userLoginValidator, userLogin);
+userRouter.put("/:id", ...userUpdateValidator, updateUserById);
+userRouter.delete("/:id", deleteUserById);
+
 export { userRouter };
