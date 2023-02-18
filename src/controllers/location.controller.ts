0475ee226ddb7b6b20fa05b36d7bb04821344640
Amrnash <amrnashaat8@gmail.com>
2023-02-18 21:21:46 +0200

    Create location controller

---
+++ src/controllers/location.controller.ts
@@ -0,0 +1,82 @@
+import { Request, Response, NextFunction } from "express";
+import { LocationService } from "../services";
+export const getAllLocations = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const locations = await LocationService.getAllLocations();
+    return res.json({ locations });
+  } catch (error) {
+    next(error);
+  }
+};
+export const getLocationById = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const id = req.params.id;
+    const location = await LocationService.getLocationById(id);
+    return res.json({ location });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const createLocation = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const { street, city, province, lat, lng } = req.body;
+    const locationData = {
+      street,
+      city,
+      province,
+      lat: Number(lat),
+      lng: Number(lng),
+    };
+    const newLocation = await LocationService.createLocation(locationData);
+    return res.json({ newLocation });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const updateLocationById = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const id = req.params.id;
+    const updateData = req.body;
+    if ("lat" in updateData) updateData.lat = Number(updateData.lat);
+    if ("lng" in updateData) updateData.lng = Number(updateData.lng);
+    const updatedLocation = await LocationService.updateLocationById(
+      id,
+      updateData
+    );
+    return res.json({ updatedLocation });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const deleteLocationById = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const id = req.params.id;
+    const deletedLocation = await LocationService.deleteLocationById(id);
+    return res.status(202).json({ deletedLocation });
+  } catch (error) {
+    next(error);
+  }
+};
