4281e513a252d1c022e8a1115321fd02dd87680d
Amrnash <amrnashaat8@gmail.com>
2023-02-18 21:26:08 +0200

    Use location router

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import express, { Express } from "express";
 import dotenv from "dotenv";
 import morgan from "morgan";
 import { errorHandler } from "./utils/errorhandler";
-import { patientRouter, doctorRouter } from "./routers";
+import { patientRouter, doctorRouter, locationRouter } from "./routers";
 dotenv.config();
 const app: Express = express();
 const port = process.env.PORT;
@@ -11,6 +11,7 @@ app.use(express.json());
 app.use(morgan("dev"));
 app.use("/patient", patientRouter);
 app.use("/doctor", doctorRouter);
+app.use("/location", locationRouter);
 app.use(errorHandler);
 app.listen(port, () => {
   console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
--- src/routers/index.ts
+++ src/routers/index.ts
@@ -1,2 +1,3 @@
 export * from "./doctor.router";
 export * from "./patient.router";
+export * from "./location.router";
