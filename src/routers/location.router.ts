a6fbeabd85504583cbfeb65a34802bf97eccbb7d
Amrnash <amrnashaat8@gmail.com>
2023-02-24 19:01:39 +0200

    Fix types in location controller

--- src/routers/location.router.ts
+++ src/routers/location.router.ts
@@ -1,18 +1,12 @@
 import { Router } from "express";
-import {
-  getAllLocations,
-  getLocationById,
-  createLocation,
-  updateLocationById,
-  deleteLocationById,
-} from "../controllers/location.controller";
+import { LocationController } from "../controllers/location.controller";
 import { createValidator, updateValidator } from "../validators/location";
 const locationRouter = Router();
 
-locationRouter.get("/", getAllLocations);
-locationRouter.get("/:id", getLocationById);
-locationRouter.post("/", ...createValidator, createLocation);
-locationRouter.put("/:id", ...updateValidator, updateLocationById);
-locationRouter.delete("/:id", deleteLocationById);
+locationRouter.get("/", LocationController.getAll);
+locationRouter.get("/:id", LocationController.getById);
+locationRouter.post("/", ...createValidator, LocationController.create);
+locationRouter.put("/:id", ...updateValidator, LocationController.updateById);
+locationRouter.delete("/:id", LocationController.deleteById);
 
 export { locationRouter };
