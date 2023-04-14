69ab71962e8855a45bb3eddc1687a12ac8a38658
Amrnash <amrnashaat8@gmail.com>
2023-04-14 06:03:35 +0200

    Remove old migration files

--- prisma/migrations/20230412031502_/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "PatientReview" ALTER COLUMN "rating" SET DATA TYPE DOUBLE PRECISION;
--- prisma/migrations/20230414035623_initial/migration.sql
+++ prisma/migrations/20230414035623_initial/migration.sql
@@ -1,8 +1,7 @@
 -- CreateTable
 CREATE TABLE "Patient" (
     "id" SERIAL NOT NULL,
-    "firstName" TEXT NOT NULL,
-    "lastName" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
     "username" TEXT NOT NULL,
     "password" TEXT NOT NULL,
     "email" TEXT NOT NULL,
@@ -19,7 +18,7 @@ CREATE TABLE "PatientReview" (
     "doctorId" INTEGER NOT NULL,
     "patientId" INTEGER NOT NULL,
     "review" TEXT NOT NULL,
-    "rating" INTEGER NOT NULL,
+    "rating" DOUBLE PRECISION NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
 
@@ -31,9 +30,8 @@ CREATE TABLE "Location" (
     "id" SERIAL NOT NULL,
     "street" TEXT NOT NULL,
     "city" TEXT NOT NULL,
-    "province" TEXT NOT NULL,
-    "lat" DECIMAL(65,30) NOT NULL,
-    "lng" DECIMAL(65,30) NOT NULL,
+    "governorate" TEXT NOT NULL,
+    "coordinates" TEXT NOT NULL,
 
     CONSTRAINT "Location_pkey" PRIMARY KEY ("id")
 );
@@ -41,14 +39,16 @@ CREATE TABLE "Location" (
 -- CreateTable
 CREATE TABLE "Doctor" (
     "id" SERIAL NOT NULL,
-    "firstName" TEXT NOT NULL,
-    "lastName" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
     "username" TEXT NOT NULL,
     "password" TEXT NOT NULL,
     "email" TEXT NOT NULL,
     "phoneNumber" TEXT NOT NULL,
     "specialization" TEXT NOT NULL,
-    "locationId" INTEGER NOT NULL,
+    "subSpecialization" TEXT NOT NULL,
+    "price1" DOUBLE PRECISION,
+    "price2" DOUBLE PRECISION,
+    "locationId" INTEGER,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
 
@@ -56,13 +56,23 @@ CREATE TABLE "Doctor" (
 );
 
 -- CreateTable
+CREATE TABLE "Moderator" (
+    "id" SERIAL NOT NULL,
+    "username" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "permissions" TEXT[],
+
+    CONSTRAINT "Moderator_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
 CREATE TABLE "Prescription" (
     "id" SERIAL NOT NULL,
-    "doctorId" INTEGER NOT NULL,
-    "patientId" INTEGER NOT NULL,
-    "appointmentId" INTEGER NOT NULL,
-    "diagnoese" TEXT NOT NULL,
-    "drugs" TEXT NOT NULL,
+    "prognosis" TEXT NOT NULL,
+    "dosage" TEXT NOT NULL,
+    "notes" TEXT NOT NULL,
+    "date" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
     CONSTRAINT "Prescription_pkey" PRIMARY KEY ("id")
 );
@@ -70,16 +80,18 @@ CREATE TABLE "Prescription" (
 -- CreateTable
 CREATE TABLE "Appointment" (
     "id" SERIAL NOT NULL,
+    "patientId" INTEGER NOT NULL,
     "doctorId" INTEGER NOT NULL,
     "type" TEXT NOT NULL,
     "at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
+    "prescriptionId" INTEGER,
 
     CONSTRAINT "Appointment_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateIndex
-CREATE UNIQUE INDEX "Doctor_locationId_key" ON "Doctor"("locationId");
+CREATE UNIQUE INDEX "Appointment_prescriptionId_key" ON "Appointment"("prescriptionId");
 
 -- AddForeignKey
 ALTER TABLE "PatientReview" ADD CONSTRAINT "PatientReview_doctorId_fkey" FOREIGN KEY ("doctorId") REFERENCES "Doctor"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
@@ -88,16 +100,13 @@ ALTER TABLE "PatientReview" ADD CONSTRAINT "PatientReview_doctorId_fkey" FOREIGN
 ALTER TABLE "PatientReview" ADD CONSTRAINT "PatientReview_patientId_fkey" FOREIGN KEY ("patientId") REFERENCES "Patient"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "Doctor" ADD CONSTRAINT "Doctor_locationId_fkey" FOREIGN KEY ("locationId") REFERENCES "Location"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE "Prescription" ADD CONSTRAINT "Prescription_doctorId_fkey" FOREIGN KEY ("doctorId") REFERENCES "Doctor"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE "Prescription" ADD CONSTRAINT "Prescription_patientId_fkey" FOREIGN KEY ("patientId") REFERENCES "Patient"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE "Prescription" ADD CONSTRAINT "Prescription_appointmentId_fkey" FOREIGN KEY ("appointmentId") REFERENCES "Appointment"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "Doctor" ADD CONSTRAINT "Doctor_locationId_fkey" FOREIGN KEY ("locationId") REFERENCES "Location"("id") ON DELETE SET NULL ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "Appointment" ADD CONSTRAINT "Appointment_doctorId_fkey" FOREIGN KEY ("doctorId") REFERENCES "Doctor"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Appointment" ADD CONSTRAINT "Appointment_patientId_fkey" FOREIGN KEY ("patientId") REFERENCES "Patient"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Appointment" ADD CONSTRAINT "Appointment_prescriptionId_fkey" FOREIGN KEY ("prescriptionId") REFERENCES "Prescription"("id") ON DELETE SET NULL ON UPDATE CASCADE;
